d72bfe7268bf6995dacf30b8fa4cbd858cc4f9e3
jacobweir <147011677+jacobweir@users.noreply.github.com>
2024-03-25 13:46:29 -0700

    Update NotificationCollection.java
    added methods

--- NotificationCollection.java
+++ NotificationCollection.java
@@ -1,4 +1,19 @@
 //NotificationCollection.java
 public class NotificationCollection {
     
+    public Notification getItem(int index){
+        return null;
+    }
+
+    public void addItem(String str){
+
+    }
+
+    public int getLength(){
+        return 0;
+    }
+
+    public void setAttachment(Attachment a, int index){
+
+    }
 }
