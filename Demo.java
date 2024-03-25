d0063ddaca61fcc0436ed443cb2d80d61a9646dd
sawyersieja <sawyersieja@protonmail.com>
2024-03-25 15:34:56 -0700

    Added provided code for main function in Demo.java.

--- Demo.java
+++ Demo.java
@@ -1,6 +1,10 @@
 //Demo.java
 public class Demo {
     public static void main(String[] args) {
-        
+        NotificationCollection nc = new NotificationCollection();
+        NotificationBar nb = new NotificationBar(nc);
+        //1. create Notifications and add them to nc
+        //2. create attachments and link them to notifications in nc
+        nb.printNotifications();
     }
 }
