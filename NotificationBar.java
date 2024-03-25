c19e026b8490acd05c8a1fd846404c39646df9c5
jacobweir <147011677+jacobweir@users.noreply.github.com>
2024-03-25 13:46:09 -0700

    Update NotificationBar.java
    added method

--- NotificationBar.java
+++ NotificationBar.java
@@ -1,4 +1,12 @@
 //NotificationBar.java
 public class NotificationBar {
     
+    public void printNotifications()
+    {
+        NotificationIteratorIF iterator = notifications.createIterator();
+        while(iterator.hasNext()){
+            Notification n = iterator.next();
+            System.out.println(n.getContent());
+        }
+    }
 }
